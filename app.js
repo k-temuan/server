7fde078b7a0edc11a95b0c205a153ac555a50279
ryanadhi <ryanadhi.p@gmail.com>
2020-04-17 10:12:35 +0700

    EARLY INITIATION
    Package installation:
    - bcryptjs
    - cors
    - express
    - jsonwebtoken
    - morgan
    - pg
    - sequelize
    ---devDependencies
    - dotenv
    - jest
    - supertest
    
    Sequelize init
    
    bin/http.js is tested

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require ('express') ;
+const app = express() ;
+const morgan = require('morgan')
+const routers = require ('./routes') ;
+const cors = require ('cors') ;
+// const errorHandler = require('./middlewares/errorHandler');
+
+app.use(morgan('dev')) ;
+app.use(cors()) ;
+app.use(express.json()) ;
+app.use(express.urlencoded({ extended: false })) ;
+
+app.use(routers) ;
+// app.use(errorHandler);
+
+module.exports = app
