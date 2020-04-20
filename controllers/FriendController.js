84d92f91ff4f0f9aaa9ec731f2b8bbe76f1f0db5
fadhilahm <fadhilahmetra@gmail.com>
2020-04-20 14:18:31 +0700

    Add authentification to UserId and FriendId

--- controllers/FriendController.js
+++ controllers/FriendController.js
@@ -3,13 +3,32 @@ const { Friend, User } = require("../models");
 
 class FriendController {
   static create(req, res, next) {
+    // check first whether or not that friend request already exist
     let { id } = req.decoded;
-    let { FriendId, status } = req.body;
-    Friend.create({
-      UserId: id,
-      FriendId,
-      status,
+    let { FriendId } = req.params;
+    let { status } = req.body;
+    Friend.findOne({
+      where: {
+        UserId: id,
+        FriendId,
+      },
     })
+      .then((found) => {
+        if (found) {
+          // duplicate exist, throw error
+          next({
+            name: "custom",
+            status: 400,
+            message: "Friend request already sent",
+          });
+        } else {
+          return Friend.create({
+            UserId: id,
+            FriendId,
+            status,
+          });
+        }
+      })
       .then((result) => {
         res.status(201).json({
           message: "Successfully created a new Friend",
@@ -21,6 +40,7 @@ class FriendController {
 
   static findAll(req, res, next) {
     let { id } = req.decoded;
+    let filtered = [];
     Friend.findAll({
       where: {
         UserId: id,
@@ -39,9 +59,18 @@ class FriendController {
       ],
     })
       .then((result) => {
+        // delete key to password
+        result.forEach((element) => {
+          let obj = {};
+          obj = { ...element["dataValues"] };
+          // deleting the password from include
+          delete obj["User"]["dataValues"]["password"];
+          delete obj["friend"]["dataValues"]["password"];
+          filtered.push(obj);
+        });
         res.status(200).json({
           message: "Successfully fetched friends data",
-          data: result,
+          data: filtered,
         });
       })
       .catch(next);
@@ -51,12 +80,30 @@ class FriendController {
     // id is the one that did the friend request first
     let { id } = req.decoded;
     // FriendId is the one that is requested for a friendship
-    let { FriendId } = req.body;
-    Friend.create({
-      UserId: id,
-      FriendId,
-      status: "pending",
+    let { FriendId } = req.params;
+    // check first whether there are already similar request
+    Friend.findOne({
+      where: {
+        UserId: id,
+        FriendId,
+      },
     })
+      .then((found) => {
+        if (found) {
+          // duplicate exist, throw error
+          next({
+            name: "custom",
+            status: 400,
+            message: "Friend request already sent",
+          });
+        } else {
+          return Friend.create({
+            UserId: id,
+            FriendId,
+            status: "pending",
+          });
+        }
+      })
       .then((result) => {
         res.status(201).json({
           message: "Friend request sent",
@@ -69,6 +116,7 @@ class FriendController {
   static getRequest(req, res, next) {
     let { id } = req.decoded;
     let filtered = null;
+    let filtered2 = [];
     // obtain raw data
     Friend.findAll({
       where: {
@@ -89,9 +137,18 @@ class FriendController {
       .then((result) => {
         // filter based on status
         filtered = result.filter(({ status }) => status === "pending");
+        // delete key to password
+        filtered.forEach((element) => {
+          let obj = {};
+          obj = { ...element["dataValues"] };
+          // deleting the password from include
+          delete obj["User"]["dataValues"]["password"];
+          delete obj["friend"]["dataValues"]["password"];
+          filtered2.push(obj);
+        });
         res.status(200).json({
           message: "Successfully fetched friends data",
-          data: filtered,
+          data: filtered2,
         });
       })
       .catch(next);
@@ -101,7 +158,7 @@ class FriendController {
     // FriendId / id = the user that is friend requested
     let { id } = req.decoded;
     // UserId = the user that initiate the friend request
-    let { UserId } = req.body;
+    let { UserId } = req.params;
     // update the friend table
     Friend.update(
       {
@@ -134,6 +191,7 @@ class FriendController {
   static getApprove(req, res, next) {
     let { id } = req.decoded;
     let filtered = null;
+    let filtered2 = [];
     // obtain raw data
     Friend.findAll({
       where: {
@@ -154,6 +212,15 @@ class FriendController {
       .then((result) => {
         // filter based on status
         filtered = result.filter(({ status }) => status === "accepted");
+        // delete key to password
+        filtered.forEach((element) => {
+          let obj = {};
+          obj = { ...element["dataValues"] };
+          // deleting the password from include
+          delete obj["User"]["dataValues"]["password"];
+          delete obj["friend"]["dataValues"]["password"];
+          filtered2.push(obj);
+        });
         res.status(200).json({
           message: "Successfully fetched friends data",
           data: filtered,
@@ -166,7 +233,7 @@ class FriendController {
     // FriendId = the user that is friend requested
     let { id } = req.decoded;
     // UserId = the user that initiate the friend request
-    let { UserId } = req.body;
+    let { UserId } = req.params;
     Friend.findOne({
       where: {
         UserId,
@@ -198,6 +265,10 @@ class FriendController {
         } else {
           res.status(200).json({
             message: "Friend request rejected",
+            data: {
+              UserId,
+              FriendId: String(id),
+            },
           });
         }
       })
--- routes/friend.js
+++ routes/friend.js
@@ -4,13 +4,27 @@ const router = require("express").Router();
 // import from controllers folder
 const FriendController = require("../controllers/FriendController");
 
+// import from middlewares folder
+const {
+  FriendIdAuthentificator,
+  UserIdAuthentificator,
+} = require("../middlewares/friendAuthentication");
+
 // main
-router.post("/request", FriendController.request);
+router.post(
+  "/request/:FriendId",
+  FriendIdAuthentificator,
+  FriendController.request
+);
 router.get("/request", FriendController.getRequest);
-router.post("/approve", FriendController.approve);
+router.post(
+  "/approve/:UserId",
+  UserIdAuthentificator,
+  FriendController.approve
+);
 router.get("/approve", FriendController.getApprove);
-router.post("/reject", FriendController.reject);
-router.post("/", FriendController.create);
+router.post("/reject/:UserId", UserIdAuthentificator, FriendController.reject);
+router.post("/:FriendId", FriendIdAuthentificator, FriendController.create);
 router.get("/", FriendController.findAll);
 
 module.exports = router;
