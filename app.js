efc9f96337fa9cd9af05d0baed19fb4e63df3f57
fadhilahm <fadhilahmetra@gmail.com>
2020-04-21 21:08:46 +0700

    Heroku config set dotenv to only work during development and testing

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const routers = require('./routes')
-const cors = require('cors')
-const errorHandler = require('./middlewares/errorHandler')
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+  require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const routers = require("./routes");
+const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandler");
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(routers)
-app.use(errorHandler)
+app.use(routers);
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
