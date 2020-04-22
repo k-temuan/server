5f94fa2a98ebf4632aaf7f7392c88124fdc4ad4a
fadhilahm <fadhilahmetra@gmail.com>
2020-04-22 16:38:24 +0700

    Finish testing using cron and local database

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -1,7 +1,9 @@
+// cron initialization
+let cron = null;
 // import from node_modules
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
-  const cron = require("node-cron");
+  cron = require("node-cron");
 }
 const moment_timezone = require("moment-timezone");
 
@@ -19,8 +21,8 @@ const start = async () => {
   let AttendeeList = AttendeeListRaw.map((el) => el["dataValues"]);
 
   // generate date.now and change it to tomorrow
-  // let comparedDate = moment_timezone("11/12/2020", "DD/MM/YYYY", true) // uncomment this for testing
-  let comparedDate = moment_timezone() // uncomment this for production
+  let comparedDate = moment_timezone("11/12/2020", "DD/MM/YYYY", true) // uncomment this for testing
+  // let comparedDate = moment_timezone() // uncomment this for production
     .add(1, "day")
     .tz("Asia/Jakarta")
     .format("l");
@@ -37,6 +39,8 @@ const start = async () => {
       });
     }
   });
+
+
   // STEP 2: Check for events that is already past their date_time and change its status to false
   let eventListRaw = await Event.findAll();
   // only get the datavalues key from Attendees
@@ -46,24 +50,70 @@ const start = async () => {
     // let comparedEventDate = moment_timezone() // uncomment this for production
     .tz("Asia/Jakarta")
     .format("l");
-  eventList.forEach(({ id, status, date_time }) => {
-    let tempDate = moment_timezone(date_time).tz("Asia/Jakarta").format("l");
-    // trigger if status === true
-    if (status && tempDate > comparedEventDate) {
-      // change status to false
-      console.log(`Updating event ${id} on ${date_time}`);
-      Event.update(
-        {
-          status: false,
-        },
-        {
-          where: {
-            id,
-          },
-        }
+  eventList.forEach(
+    ({
+      id,
+      status,
+      date_time,
+      name,
+      category,
+      description,
+      max_attendees,
+      image_url,
+      location,
+      UserId,
+    }) => {
+      let tempDate = moment_timezone(date_time).tz("Asia/Jakarta").format("l");
+      // trigger if status === true
+      console.log(
+        `THIS IS THE ONE IN THE DATABASE",
+        id : ${id},
+        status : ${status}, 
+        date_time : ${date_time},
+        name : ${name},
+        category : ${category},
+        description : ${description},
+        max_attendees : ${max_attendees},
+        image_url : ${image_url},
+        location : ${location},
+        UserId : ${UserId},
+        tempDate : ${tempDate}
+        `
       );
+      console.log(`
+      THIS IS THE COMPARED DATE", 
+      comparedEventDate : ${comparedEventDate}
+      `);
+      if (status && tempDate > comparedEventDate) {
+        // change status to false
+        console.log(`
+        ------------------------------------
+        Updating event ${id} on ${date_time}
+        ------------------------------------
+        `);
+        Event.update(
+          {
+            status: false,
+            name,
+            category,
+            description,
+            max_attendees,
+            image_url,
+            location,
+            date_time,
+            UserId,
+          },
+          {
+            where: {
+              id,
+            },
+          }
+        );
+      }
     }
-  });
+  );
 };
 
-start();
+cron.schedule("* * * * *", () => {
+  start();
+});
